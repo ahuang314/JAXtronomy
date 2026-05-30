a8ae8e86b8dab5a562d4e6776d752d58e4d6c4cf
ahuang314 <alan.huang.1@stonybrook.edu>
2026-05-29 20:49:19 -0400

    add tests for pso

---
+++ test/test_Sampling/test_Samplers/test_pso.py
@@ -0,0 +1,49 @@
+__author__ = "ahuang314"
+
+from functools import partial
+from jax import jit, lax, numpy as jnp, vmap
+import numpy as np
+import numpy.testing as npt
+import pytest
+
+from jaxtronomy.Sampling.Samplers.pso import ParticleSwarmOptimizer
+from jaxtronomy.Sampling.Samplers.pso_jit import ParticleSwarmOptimizerJIT
+
+@jit
+@vmap
+def logL(x):
+    # Minimum at x = 0.6
+    return -jnp.sum((x - 0.6) ** 2)
+
+class TestPSO(object):
+    """Tests the PSO class with a simple logL function"""
+
+    def setup_method(self):
+        args_lower = np.array([0.0])
+        args_upper = np.array([10.1])
+        args = (args_lower, args_upper)
+        self.pso = ParticleSwarmOptimizer(logL, *args, particle_count=100)
+
+    def test_run(self):
+        # Tests to see if the PSO gets close to the true answer
+        final_result, _ = self.pso.optimize(max_iter=200)
+        npt.assert_array_almost_equal(final_result, [0.6], decimal=5)
+
+class TestPSOJIT(object):
+    """Tests the PSO JIT class with a simple logL function"""
+
+    def setup_method(self):
+        args_lower = np.array([0.0])
+        args_upper = np.array([10.1])
+        args = (args_lower, args_upper)
+        self.pso = ParticleSwarmOptimizerJIT(logL, *args, particle_count=100)
+
+    def test_run(self):
+        # Tests to see if the PSO gets close to the true answer
+        final_result, _ = self.pso.optimize(max_iter=200)
+        npt.assert_array_almost_equal(final_result, [0.6], decimal=5)
+
+
+
+if __name__ == "__main__":
+    pytest.main()
